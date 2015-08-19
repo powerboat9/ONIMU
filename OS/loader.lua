96f4095aedb077276ad60684020e18508d176328
powerboat9 <powerboat9@users.noreply.github.com>
2015-08-18 21:50:22 -0400

    Update loader.lua

--- OS/loader.lua
+++ OS/loader.lua
@@ -2,3 +2,13 @@ os.loadAPI("lib/graphics")
 
 local pre_int = fs.find("programs/*/os/startup/pre_int/*")
 local post_int = fs.find("programs/*/os/startup/post_int/*")
+
+for k, v in ipairs(pre_int) do
+    shell.run(v)
+end
+
+--TODO: Add OS init
+
+for k, v in ipairs(post_int) do
+    shell.run(v)
+end
