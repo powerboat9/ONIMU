58065e68f3c800626fc07334bdd9caff3e03a5bc
powerboat9 <powerboat9@users.noreply.github.com>
2015-09-01 12:13:57 -0400

    Added a bunch of functions

--- ONIMU/graphics.lua
+++ ONIMU/graphics.lua
@@ -1,2 +1,12 @@
-function textWithBorder(x, y, txt, border, color)
+function centerLeft(txt, paddingChar)
+    local txtLines = string.gmatch(txt, "\n")
+    local txtCentered = {}
+    for k, v in ipairs(txtLines) do
+        txtCentered[k] = v
+    return string.concat()
+
+function textWithBorder(x, y, txt, borderSize, borderColor, borderTxt, separateLinesWithBorder)
+    local txtLines = string.gmatch(txt, "\n")
+    borderTxt = borderTxt or " "
     local rectX = x - border
+    local
