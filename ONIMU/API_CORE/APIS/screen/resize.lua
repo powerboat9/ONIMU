063027907e971e0318fd23f0cdb88d658e54cb8f
powerboat9 <powerboat9@users.noreply.github.com>
2015-10-17 21:35:14 -0400

    Update resize.lua

--- ONIMU/API_CORE/APIS/screen/resize.lua
+++ ONIMU/API_CORE/APIS/screen/resize.lua
@@ -3,5 +3,15 @@ sizeTerm = {
     termY1 = 0,
     termX2 = 0,
     termY2 = 0,
+    
     setCursorPos = function(self, cursorX, cursorY)
-        newPosX = termX1 + cursor
+        newPosX = termX1 + cursorX
+        newPosY = termY1 + cursorY
+        if newPosX <= termX2 and newPosY <= termY2 then
+            term.setCursorPos(newPosX, newPosY)
+        end
+        return nil
+    end,
+    
+    blit = function(txt, color, backround_color)
+        
